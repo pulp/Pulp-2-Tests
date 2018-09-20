148b6d42bb03bcf7e03a9b32300fedd210cb3920
kersom <kersom@gmail.com>
2018-09-20 12:52:40 -0400

    Remove extra line wrapping
    Remove extra line wrapping and use of parenthesis. Minor clean up.

--- pulp_2_tests/tests/rpm/api_v2/test_modular_errata.py
+++ pulp_2_tests/tests/rpm/api_v2/test_modular_errata.py
@@ -5,13 +5,9 @@ import unittest
 from packaging.version import Version
 from pulp_smash import api, config
 from pulp_smash.pulp2.constants import REPOSITORY_PATH
-from pulp_smash.pulp2.utils import (
-    sync_repo,
-)
+from pulp_smash.pulp2.utils import sync_repo
 
-from pulp_2_tests.constants import (
-    RPM_WITH_MODULES_FEED_URL,
-)
+from pulp_2_tests.constants import RPM_WITH_MODULES_FEED_URL
 from pulp_2_tests.tests.rpm.api_v2.utils import (
     gen_distributor,
     gen_repo,
