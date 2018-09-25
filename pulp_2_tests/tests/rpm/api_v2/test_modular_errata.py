f27957e95b460821d6b91d33a54cb83d63eaca19
kersom <kersom@gmail.com>
2018-09-25 12:46:10 -0400

    Update modular errata tests
    Update the newly added modular errata tests.
    
    * Wrap lines length
    * Update docstrings
    * Use defaultdict
    
    See: https://github.com/PulpQE/Pulp-2-Tests/commit/76bfab6dcbe809c9d840412a9eecf027e6a92a83

--- pulp_2_tests/tests/rpm/api_v2/test_modular_errata.py
+++ pulp_2_tests/tests/rpm/api_v2/test_modular_errata.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 """Tests that perform actions over modular Errata repositories."""
+from collections import defaultdict
 import unittest
 
 from packaging.version import Version
@@ -19,9 +20,11 @@ from pulp_2_tests.tests.rpm.api_v2.utils import (
 class ManageModularErrataTestCase(unittest.TestCase):
     """Manage Modular Errata content testcase.
 
-    This test targets the following issue.
+    This test targets the following issues:
 
-    `Pulp-2-Tests #94 <https://github.com/PulpQE/Pulp-2-Tests/issues/94>`_.
+    * `Pulp-2-Tests #94 <https://github.com/PulpQE/Pulp-2-Tests/issues/94>`_.
+    * `Pulp #3919 <https://pulp.plan.io/issues/3919>`_
+
     """
 
     @classmethod
@@ -36,13 +39,14 @@ class ManageModularErrataTestCase(unittest.TestCase):
         """Test sync,publish of Modular RPM repo and checks the update info.
 
         This testcase reads the updateinfo of the repository that is published
-        and compares that against the updateinfo.xml present in the rpm url.
+        and compares that against the ``updateinfo.xml`` present in the feed
+        url.
 
         Steps involved:
 
         1. Create a repository with feed url containing modules.
         2. The repository should have a distributor to publish it.
-        3. Once the repository is created, it is sync and published.
+        3. Once the repository is created, it is synced and published.
         4. Get the ``updateinfo`` from the repodata of the published repo.
         5. Compare this against the ``update_info.xml`` in the fixtures repo.
         """
@@ -62,41 +66,50 @@ class ManageModularErrataTestCase(unittest.TestCase):
     def test_collection_field(self):
         """Test the collection field in the update info.
 
-        This test provides the following
+        This test provides the following:
 
-        1. Check whether all the modules in the published repo
-            contains a collection field.
-        2. Check whether the collection field has proper name. The collection name computation
-            is as below.
+        1. Check whether all the modules in the published repo contains a
+           collection field.
+        2. Check whether the collection field has proper name. The collection
+           name computation is as below.
 
-        The collection name is created using the information from fixtures that is stored in
-        a set {<errata-id>:<module-name>}. First, the set information is used in computing
-        a set ``collections_from_fixtures`` that maps the repo_id to the collection-name.
-        The collection-name set is computed using the logic <repo-id>_<index>_<module-name>.
-        The module name is ``default`` and the index is``0`` for ursine RPMs. The set is
-        created using set-comprehension and x-path. After creating the set,
-        it appears as in the example below.
+        The collection name is created using the information from fixtures that
+        is stored in a set ``{<errata-id>:<module-name>}``.
 
-        example :
-        collections_from_fixtures = {
-            'RHEA..1' : 'repo_id_1_duck',
-            'RHEA..2' : 'repo_id_2_duck',
-            'RHEA..3' : 'repo_id_0_default'
-        }
+        First, the set information is used in computing a set
+        ``collections_from_fixtures`` that maps the repo_id to the
+        collection-name.
 
-        This set is compared against the collection-name from the published repo's ``updateinfo``.
+        The collection-name set is computed using the logic
+        ``<repo-id>_<index>_<module-name>``.  The module name is ``default``
+        and the index is 0 for ursine RPMs.
+
+        The set is created using set-comprehension and x-path. After creating
+        the set, it appears as in the example below.
+
+        .. code:: python
+
+            collections_from_fixtures = {
+                'RHEA..1' : 'repo_id_1_duck',
+                'RHEA..2' : 'repo_id_2_duck',
+                'RHEA..3' : 'repo_id_0_default'
+            }
+
+        This set is compared against the collection-name from the published
+        repo's ``updateinfo``.
         """
         repo, update_list = self._set_repo_and_get_repo_data()
 
-        # getting the update info from the fixtures repo
+        # getting the updateinfo from the fixtures repo
         update_info_fixtures = get_xml_content_from_fixture(
             fixture_path=RPM_WITH_MODULES_FEED_URL,
             data_type='updateinfo',
         )
 
-        # Errata ID to Collection name map in Updateinfo of published repo.
+        # Errata ID to collection name map in updateinfo of published repo.
         collection_update_list = {
-            update.find('./id').text: update.find('.//collection').attrib['short']
+            update.find('./id').text:
+            update.find('.//collection').attrib['short']
             for update in update_list.findall('update')
         }
 
@@ -108,13 +121,12 @@ class ManageModularErrataTestCase(unittest.TestCase):
             update_info_fixtures.findall('.//update')
         }
 
-        # ``indexes`` is used to increase the index of the module in the collections
-        indexes = {}
-        for key, val in collections_from_fixtures.items():
+        # indexes is used to increase the index of the module in the
+        # collections
+        indexes = defaultdict(lambda: 1)
+        for key, val in tuple(collections_from_fixtures.items()):
             if val in indexes:
                 indexes[val] += 1
-            else:
-                indexes[val] = 1
             collections_from_fixtures[key] = (
                 '{}_0_default'.format(repo['id'])
                 if val == 'default'
@@ -132,12 +144,12 @@ class ManageModularErrataTestCase(unittest.TestCase):
 
         This method does the following:
 
-        1. Create, Sync and Publish a repo with
+        1. Create, sync and publish a repo with
             ``RPM_WITH_MODULES_FEED_URL``
-        2. Get ``updateinfo`` xml of the published repo.
+        2. Get ``updateinfo.xml`` of the published repo.
 
         :returns: A tuple containing the repo that is created, along with
-            the ``updateinfo`` xml of the created repo.
+            the ``updateinfo.xml`` of the created repo.
         """
         body = gen_repo(
             importer_config={'feed': RPM_WITH_MODULES_FEED_URL},
@@ -147,7 +159,7 @@ class ManageModularErrataTestCase(unittest.TestCase):
         self.addCleanup(self.client.delete, repo['_href'])
         sync_repo(self.cfg, repo)
 
-        # getting the update info from the published repo
+        # getting the updateinfo from the published repo
         repo = self.client.get(repo['_href'], params={'details': True})
         return repo, get_repodata(
             self.cfg,
@@ -158,6 +170,7 @@ class ManageModularErrataTestCase(unittest.TestCase):
     def _get_errata_rpm_mapping(xml):
         mapper = {}
         for update in xml.findall('update'):
-            temp = [package.text for package in update.findall('.//filename')]
-            mapper[update.find('id').text] = temp
+            mapper[update.find('id').text] = [
+                package.text for package in update.findall('.//filename')
+            ]
         return mapper
