5f1616a27a522e5a82e0c79e095d6b80ce5304e4
Brian Herring <bherring@redhat.com>
2018-11-03 14:51:40 -0400

    Related: #77 Puppet/api_v2 Refactor
    Addresses some BaseAPITestCase refactoring.
    
    Refactor Locations updated:
    	- tests/puppet/api_v2

--- pulp_2_tests/tests/puppet/api_v2/test_duplicate_uploads.py
+++ pulp_2_tests/tests/puppet/api_v2/test_duplicate_uploads.py
@@ -14,24 +14,34 @@ The second upload should silently fail for all Pulp releases in the 2.x series.
 .. _Pulp #1406: https://pulp.plan.io/issues/1406
 .. _Pulp Smash #81: https://github.com/PulpQE/pulp-smash/issues/81
 """
-from pulp_smash import api, utils
+import unittest
+
+from pulp_smash import api, utils, config
 from pulp_smash.pulp2.constants import REPOSITORY_PATH
-from pulp_smash.pulp2.utils import BaseAPITestCase, DuplicateUploadsMixin
+from pulp_smash.pulp2.utils import DuplicateUploadsMixin
 
 from pulp_2_tests.constants import PUPPET_MODULE_URL_1
 from pulp_2_tests.tests.puppet.api_v2.utils import gen_repo
 from pulp_2_tests.tests.puppet.utils import set_up_module as setUpModule  # pylint:disable=unused-import
 
 
-class DuplicateUploadsTestCase(BaseAPITestCase, DuplicateUploadsMixin):
+class DuplicateUploadsTestCase(unittest.TestCase, DuplicateUploadsMixin):
     """Test how well Pulp can deal with duplicate content unit uploads."""
 
     @classmethod
     def setUpClass(cls):
         """Create a Puppet repository."""
-        super().setUpClass()
+        cls.cfg = config.get_config()
+        cls.resources = set()
         unit = utils.http_get(PUPPET_MODULE_URL_1)
         import_params = {'unit_type_id': 'puppet_module'}
-        repo = api.Client(cls.cfg).post(REPOSITORY_PATH, gen_repo()).json()
+        cls.client = api.Client(cls.cfg, api.json_handler)
+        repo = cls.client.post(REPOSITORY_PATH, gen_repo())
         cls.upload_import_unit_args = (cls.cfg, unit, import_params, repo)
         cls.resources.add(repo['_href'])
+
+    @classmethod
+    def tearDownClass(cls):
+        """Delete all resources named by ``resources``."""
+        for resource in cls.resources:
+            cls.client.delete(resource)
